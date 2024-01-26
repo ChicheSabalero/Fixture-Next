9fab2f2ce6317fad572cae52e55a7c218de64579
ChicheSabalero <71633336+ChicheSabalero@users.noreply.github.com>
2024-01-26 02:31:37 +0100

    Equipos Cargados

--- src/db/getDb.js
+++ src/db/getDb.js
@@ -11,7 +11,14 @@ async function connectToDatabase() {
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_NAME,
+      database: process.env.MYSQL_NAME || "Fixture",
     });
+
+    // Crear la base de datos "Fixture" si no existe
+    await connection.query("CREATE DATABASE IF NOT EXISTS Fixture");
+
+    // Cambiar a la base de datos "Fixture"
+    await connection.query("USE Fixture");
 
     console.log("Conexión a MySQL establecida correctamente");
     return connection;
@@ -19,6 +26,7 @@ async function connectToDatabase() {
     console.error("Error al conectar a MySQL:", error);
     throw error; // Propaga el error para que pueda ser manejado por el llamador
   }
+  console.log("Conectado");
 }
 
 module.exports = connectToDatabase;
