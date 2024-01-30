6c9319442a7031f61ea27e3fe3c67120d98dfc64
ChicheSabalero <71633336+ChicheSabalero@users.noreply.github.com>
2024-01-30 13:20:16 +0100

    Equipos cargados

--- src/db/initDb.js
+++ src/db/initDb.js
@@ -1,81 +1,74 @@
 require("dotenv").config();
 
-const getDb = require("../db/getDb");
+const { Query } = require("mysql2");
+const getDb = require("./getDb");
 
 const main = async () => {
   let connection;
 
   try {
     connection = await getDb();
+
     await connection.query(`
-    CREATE TABLE Equipos (
-      Id VARCHAR(2) UNIQUE PRIMARY KEY,
-      Equipo VARCHAR(255),
-      Zona CHAR(1),
-      puntos INT,
-      PJ INT,
-      PG INT,
-      PE INT,
-      PP INT,
-      GF INT,
-      GC INT,
-      DG INT
+
+    CREATE TABLE IF NOT EXISTS Equipos (
+      Id VARCHAR(2) UNIQUE PRIMARY KEY, Equipo VARCHAR(255), Zona CHAR(1), puntos INT, PJ INT, PG INT, PE INT, PP INT, GF INT, GC INT, DG INT
   );`);
+
+    // await connection.query(`USE Fixture`);
+
     await connection.query(`
     
-  INSERT INTO Equipos (Id, Equipo, Zona, puntos, PJ, PG, PE, PP, GF, GC, DG)
-  VALUES
-      ('01', 'Agropecuario', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('02', 'Aldosivi', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('03', 'All Boys', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('04', 'Almagro', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('05', 'Almirante Brown SJ', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('06', 'Alvarado MdP', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('07', 'Arsenal', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('08', 'A Rafaela', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('09', 'Brown A', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('10', 'Castellano', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('11', 'Chacarita', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('12', 'Chaco FE', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('13', 'Colón', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('14', 'Def Belgrano', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('15', 'Def Unidos', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('16', 'Dep Maipú', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('17', 'Dep Madryn', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('18', 'Dep Morón', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('19', 'Estudiantes CA', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('20', 'Estudiantes RC', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('21', 'Ferro Carril Oeste', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('22', 'GyE M', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('23', 'GyE J', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('24', 'GyT S', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('25', 'Brown PM', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('26', 'Güemes SE', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('27', 'Nueva Chicago', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('28', 'Patronato', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('29', 'Quilmes', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('30', 'Racing C', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('31', 'San Martín SJ', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('32', 'San Martín T', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('33', 'San Telmo', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('34', 'San Miguel', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('35', 'Temperley', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('36', 'Talleres RE', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('37', 'Tristán Suárez', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
-      ('38', 'Atlanta', 'B', 0, 0, 0, 0, 0, 0, 0, 0);
-    
+    INSERT INTO
+    Equipos (Id, Equipo, Zona, puntos, PJ, PG, PE, PP, GF, GC, DG)
+    VALUES ('01', 'Agropecuario', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('03', 'All Boys', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('05', 'Alvarado', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('07', 'Arsenal', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('09', 'Brown PM)', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('11', 'Chacarita', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('13', 'Dep. Maipu', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('15', 'Estudiantes BA', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('17', 'Ferro', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('19', 'Gimnasia J', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('21', 'Guemes SdE', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('23', 'Patronato', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('25', 'Racing C', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('27', 'San Martin SJ', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('29', 'San Martin T', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('31', 'San Miguel', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('33', 'Talleres RdE', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('35', 'Tristan Suarez', 'A', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('37', 'Quilmes*', 'A', -3, 0, 0, 0, 0, 0, 0, 0);
+
     `);
+
     await connection.query(`
-    CREATE TABLE Partidos (
-      Id VARCHAR(3) UNIQUE PRIMARY KEY,
-      Local VARCHAR(255),
-      Visitante VARCHAR(255),
-      GolL INT,
-      GolV INT,
-      ZonaL CHAR(1),
-      ZonaV CHAR(1)
-      );
+    
+    INSERT INTO
+    Equipos (Id, Equipo, Zona, puntos, PJ, PG, PE, PP, GF, GC, DG)
+    VALUES ('02', 'Aldosivi', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('04', 'Almagro', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('06', 'Alte Brown', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('08', 'Atl Rafaela', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('10', 'Atlanta', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('12', 'Brown A', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('14', 'Chaco For Ever', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('16', 'Colon', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('18', 'Def. Unidos', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('20', 'Def. de Belgrano', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('22', 'Dep. Madryn', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('24', 'Dep. Moron', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('26', 'Estudiantes RC', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('28', 'Gimnasia M', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('30', 'Gimnasia S', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('32', 'Mitre SdE', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('34', 'Nueva Chicago', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('36', 'San Telmo', 'B', 0, 0, 0, 0, 0, 0, 0, 0),
+    ('38', 'Temperley', 'B', 0, 0, 0, 0, 0, 0, 0, 0);
+      
     `);
+
     console.log("tablas creadas");
   } catch (err) {
     console.error(err);
---
+++ src/db/tablas/equipos.sql
@@ -0,0 +1,129 @@
+CREATE TABLE Equipos (
+    Id VARCHAR(2) UNIQUE PRIMARY KEY, Equipo VARCHAR(255), Zona CHAR(1), puntos INT, PJ INT, PG INT, PE INT, PP INT, GF INT, GC INT, DG INT
+);
+
+-- Para la Zona A
+INSERT INTO
+    Equipos (
+        Id, Equipo, Zona, puntos, PJ, PG, PE, PP, GF, GC, DG
+    )
+VALUES (
+        '01', 'Agropecuario', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '03', 'All Boys', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '05', 'Alvarado', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '07', 'Arsenal', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '09', 'Brown PM)', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '11', 'Chacarita', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '13', 'Dep. Maipu', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '15', 'Estudiantes BA', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '17', 'Ferro', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '19', 'Gimnasia J', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '21', 'Guemes SdE', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '23', 'Patronato', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '25', 'Racing C', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '27', 'San Martin SJ', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '29', 'San Martin T', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '31', 'San Miguel', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '33', 'Talleres RdE', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '35', 'Tristan Suarez', 'A', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '37', 'Quilmes*', 'A', -3, 0, 0, 0, 0, 0, 0, 0
+    );
+
+-- Para la Zona B
+INSERT INTO
+    Equipos (
+        Id, Equipo, Zona, puntos, PJ, PG, PE, PP, GF, GC, DG
+    )
+VALUES (
+        '02', 'Aldosivi', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '04', 'Almagro', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '06', 'Alte Brown', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '08', 'Atl Rafaela', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '10', 'Atlanta', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '12', 'Brown A', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '14', 'Chaco For Ever', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '16', 'Colon', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '18', 'Def. Unidos', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '20', 'Def. de Belgrano', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '22', 'Dep. Madryn', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '24', 'Dep. Moron', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '26', 'Estudiantes RC', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '28', 'Gimnasia M', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '30', 'Gimnasia S', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '32', 'Mitre SdE', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '34', 'Nueva Chicago', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '36', 'San Telmo', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    ),
+    (
+        '38', 'Temperley', 'B', 0, 0, 0, 0, 0, 0, 0, 0
+    );
