d510f6f67f94976a0430ad26eeb9312a86820866
ChicheSabalero <71633336+ChicheSabalero@users.noreply.github.com>
2024-02-01 07:28:18 +0100

    Bootstrap

--- src/app/fechas/page.jsx
+++ src/app/fechas/page.jsx
@@ -5,28 +5,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className="container vh-100 align-content-center">
         <table className="table">
-          <caption>Tabla de Partidos</caption>
+          {/* <caption>Tabla de Partidos</caption> */}
           <thead>
             <tr className="table-dark">
               <th scope="col">Id</th>
               <th scope="col">Fecha</th>
-              <th scope="col">Zona Local</th>
+              <th scope="col">Zona </th>
               <th scope="col">Local</th>
-              <th scope="col">Gol Local</th>
-              <th scope="col">Gol Visitante</th>
+              <th scope="col">Gol </th>
+              <th scope="col">Gol </th>
               <th scope="col">Visitante</th>
-              <th scope="col">Zona Visitante</th>
+              <th scope="col">Zona </th>
             </tr>
           </thead>
           <tbody>
             <tr class="table-secondary">
               <th scope="row">1</th>
-              <td>20240201</td>
+              <td>1</td>
               <td>A</td>
               <td>EquipoLocal1</td>
-              <td input="0">2</td>
+              <td>2</td>
               <td>1</td>
               <td>EquipoVisitante1</td>
               <td>B</td>
